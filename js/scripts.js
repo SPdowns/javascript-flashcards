4786ebdc93b39ecc76fdef6265251802c9378187
James Henager <jameschenager@gmail.com>
2020-06-02 15:57:39 -0700

    initial commit. after finishing the project we realized we didn't commit anything. whoops
    Co-authored-by: Sean Downs <spdowns@protonmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  $("#card1 h3").click(function() {
+    $("#p1").toggle();
+  });
+  $("#card2 h3").click(function() {
+    $("#p2").toggle();
+  });
+  $("#card3 h3").click(function() {
+    $("#p3").toggle();
+  });
+  $("#card4 h3").click(function() {
+    $("#p4").toggle();
+  });
+  $("#card5 h3").click(function() {
+    $("#p5").toggle();
+  });
+  $("#card6 h3").click(function() {
+    $("#p6").toggle();
+  });
+  $("#card7 h3").click(function() {
+    $("#p7").toggle();
+  });
+  $("#card8 h3").click(function() {
+    $("#p8").toggle();
+  });
+  $("#card9 h3").click(function() {
+    $("#p9").toggle();
+  });
+  $("#card10 h3").click(function() {
+    $("#p10").toggle();
+  });
+  $("#card11 h3").click(function() {
+    $("#p11").toggle();
+  });
+  $("#card12 h3").click(function() {
+    $("#p12").toggle();
+  });
+  $("#card13 h3").click(function() {
+    $("#p13").toggle();
+  });
+  $("#card14 h3").click(function() {
+    $("#p14").toggle();
+  });
+  $("#card15 h3").click(function() {
+    $("#p15").toggle();
+  });
+  $("#card16 h3").click(function() {
+    $("#p16").toggle();
+  });
+  $("#card17 h3").click(function() {
+    $("#p17").toggle();
+  });
+  $("#card18 h3").click(function() {
+    $("#p18").toggle();
+  });
+  $("#card19 h3").click(function() {
+    $("#p19").toggle();
+  });
+});
+
